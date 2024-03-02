baa4dff51c211ef893465171d41b7e176ee2e068
Hernan Case <hernan.case@deptagency.com>
2024-03-02 11:00:41 -0300

    add ToggleSwitch component

--- src/components/ToggleSwitch/index.tsx
+++ src/components/ToggleSwitch/index.tsx
@@ -1,16 +1,32 @@
-interface Props {
-  value: string;
+import { FC } from "react";
+import { Switch } from "@headlessui/react";
+import classNames from "classnames";
+
+interface ToggleSwitchProps {
   checked: boolean;
   onChange: () => void;
-  name: string;
-  id: string;
-  disabled?: boolean;
-  title: string;
-  size: string;
 }
 
-const ToggleSwitch = ({ value, checked, onChange, name, id, disabled, title, size = "lg" }: Props) => {
-  return ToggleSwitch;
+const ToggleSwitch: FC<ToggleSwitchProps> = ({ checked, onChange }) => {
+  return (
+    <Switch
+      checked={checked}
+      onChange={onChange}
+      className={classNames(
+        checked ? "bg-indigo-600" : "bg-gray-200",
+        "relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2"
+      )}
+    >
+      <span className="sr-only">Use setting</span>
+      <span
+        aria-hidden="true"
+        className={classNames(
+          checked ? "translate-x-5" : "translate-x-0",
+          "pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out"
+        )}
+      />
+    </Switch>
+  );
 };
 
 export default ToggleSwitch;
