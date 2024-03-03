b63e0a9ece73e78185ef836455e3501b77facf05
Hernan Case <hercase92@gmail.com>
2024-03-03 02:31:14 -0300

    add Rounded top

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ const Layout = ({
       <header className="grid place-items-center">
         <Logo dark />
       </header>
-      <main className="flex flex-col items-center h-full bg-gradient-to-b from-primary-900 to-primary-950 max-w-screen-lg mx-auto w-full">
+      <main className="flex flex-col items-center h-full bg-gradient-to-b from-primary-900 to-primary-950 max-w-screen-lg mx-auto w-full rounded-t-md">
         {children}
       </main>
     </body>
