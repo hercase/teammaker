8193c5793d4dec92f7d3aefc6153008a195fe6c2
Hernan Case <hernan.case@deptagency.com>
2024-03-05 19:52:56 -0300

    add FloatingMenu to PlayerList

---
+++ src/components/FloatingMenu/index.tsx
@@ -0,0 +1,65 @@
+import { FC, useState } from "react";
+import { useLayer } from "react-laag";
+
+interface FloatingMenuProps {
+  trigger: React.ReactNode;
+  children: React.ReactNode;
+  className?: string;
+}
+
+const FloatingMenu: FC<FloatingMenuProps> = ({ trigger, children, className }) => {
+  const [isOpen, setOpen] = useState(false);
+
+  // helper function to close the menu
+  function close() {
+    setOpen(false);
+  }
+
+  const { renderLayer, triggerProps, layerProps } = useLayer({
+    isOpen,
+    onOutsideClick: close,
+    onDisappear: close,
+    overflowContainer: false,
+    auto: true,
+    placement: "bottom-end",
+    triggerOffset: 2,
+    containerOffset: 16,
+  });
+
+  return (
+    <>
+      <button className={className} {...triggerProps} onClick={() => setOpen(!isOpen)}>
+        {trigger}
+      </button>
+      {renderLayer(
+        <>
+          {isOpen && (
+            <ul className="bg-white text-gray-600 border border-gray-200 rounded-md shadow-lg z-10" {...layerProps}>
+              {children}
+            </ul>
+          )}
+        </>
+      )}
+    </>
+  );
+};
+
+interface MenuOptionProps {
+  icon: React.ReactNode;
+  label: string;
+  onClick?: () => void;
+}
+
+const MenuOption: FC<MenuOptionProps> = ({ icon, label, onClick }) => (
+  <li
+    className="flex items-center gap-2 px-5 py-2 text-sm leading-6 text-gray-900 border-b border-gray-200 last-of-type:border-none cursor-pointer
+      hover:bg-gray-100"
+    onClick={onClick}
+  >
+    {icon}
+    {label}
+  </li>
+);
+
+export { MenuOption };
+export default FloatingMenu;
--- src/components/PlayersList/index.tsx
+++ src/components/PlayersList/index.tsx
@@ -1,8 +1,10 @@
 import { FC } from "react";
 import { Player } from "@/types";
-import { ShirtIcon } from "@/components/Icons";
 import { uniqueId } from "lodash";
 import classNames from "classnames";
+import { ShirtIcon } from "@/components/Icons";
+import FloatingMenu, { MenuOption } from "@/components/FloatingMenu";
+import { ArrowsUpDownIcon, EllipsisVerticalIcon, TrashIcon } from "@heroicons/react/20/solid";
 
 interface PlayersListProps {
   shirtPosition?: "left" | "right";
@@ -13,7 +15,7 @@ interface PlayersListProps {
 const PlayersList: FC<PlayersListProps> = ({ shirtPosition = "left", color = "#151d65", players }) => (
   <div className="relative w-1/2 bg-white rounded-md p-2">
     <label
-      className={classNames("flex justify-start", {
+      className={classNames("flex justify-start mb-2", {
         "justify-end": shirtPosition === "right",
       })}
     >
@@ -22,15 +24,22 @@ const PlayersList: FC<PlayersListProps> = ({ shirtPosition = "left", color = "#1
 
     <ul className="divide-y divide-gray-200">
       {players?.map((player) => (
-        <li
+        <FloatingMenu
           key={uniqueId(`${player.name}-${player.details}`)}
-          className="py-1 flex gap-1 font-display text-[16px] p-1 my-2 capitalize justify-center items-center text-gray-600"
+          className="relative py-1 flex gap-1 font-display text-[16px] p-1 py-2 capitalize justify-center items-center text-gray-600 w-full group hover:bg-gray-200"
+          trigger={
+            <>
+              <span>{player.name}</span>
+              {player.details && (
+                <span className="text-[10px] font-semibold uppercase text-secondary-600">({player.details})</span>
+              )}
+              <EllipsisVerticalIcon className="h-5 w-5 absolute right-2 top-1/2 transform -translate-y-1/2 hidden group-hover:block" />
+            </>
+          }
         >
-          <span>{player.name}</span>
-          {player.details && (
-            <span className="text-[10px] font-semibold uppercase text-secondary-600">({player.details})</span>
-          )}
-        </li>
+          <MenuOption icon={<ArrowsUpDownIcon className="h-5 w-5 fill-primary-800" />} label="Reemplazar" />
+          <MenuOption icon={<TrashIcon className="h-5 w-5 fill-red-800" />} label="Eliminar" />
+        </FloatingMenu>
       ))}
     </ul>
   </div>
