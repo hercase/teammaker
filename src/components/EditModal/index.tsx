a93bc108c3f76484a1ccf653359af1c418510e0f
Hernan Case <hernan.case@deptagency.com>
2024-03-13 11:38:41 -0300

    fix Similar blocks of code found in 2 locations

--- src/components/EditModal/index.tsx
+++ src/components/EditModal/index.tsx
@@ -42,32 +42,25 @@ const EditModal: FC<EditModalProps> = ({ isOpen, setIsOpen }) => {
     setIsOpen(false);
   };
 
+  const transitions = {
+    enter: "ease-out duration-300",
+    leave: "ease-in duration-200",
+    enterFrom: "opacity-0 scale-95",
+    enterTo: "opacity-100 scale-100",
+    leaveFrom: "opacity-100 scale-100",
+    leaveTo: "opacity-0 scale-95",
+  };
+
   return (
     <Transition appear show={isOpen} as={Fragment}>
       <Dialog as="div" className="relative z-10" onClose={() => setIsOpen(false)}>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
+        <Transition.Child as={Fragment} {...transitions}>
           <div className="fixed inset-0 bg-black/25" />
         </Transition.Child>
 
         <div className="fixed inset-0 overflow-y-auto">
           <div className="flex min-h-full items-center justify-center p-4 text-center">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 scale`-100"
-              leaveTo="opacity-0 scale-95"
-            >
+            <Transition.Child as={Fragment} {...transitions}>
               <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                 <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
                   Editar
