0482deaeed57bd70dccdabf91faa1f5b7e7fb922
Hernan Case <hercase92@gmail.com>
2024-03-25 23:35:37 -0300

    add Sanity queries

--- src/sanity/queries.ts
+++ src/sanity/queries.ts
@@ -13,11 +13,40 @@ export async function getMatch(id: string): Promise<MatchType & SanityFields> {
       _updatedAt,
       date,
       location,
-      organizer->{nickname},
+      organizer->,
       players,
       random,
       colors  
     }`,
     { id }
   );
+}
+
+export async function getMatchesByOrganizer(organizerId: string): Promise<(MatchType & SanityFields)[]> {
+  return client.fetch(
+    groq`*[_type == "match" && organizer._ref == $organizerId]{
+      _id,
+      _type,
+      _createdAt,
+      _updatedAt,
+      date,
+      location,
+      organizer->,
+      players,
+      random,
+      colors  
+    }`,
+    { organizerId }
+  );
+}
+
+export async function createMatch(data: MatchType): Promise<MatchType & SanityFields> {
+  return client.create({
+    _type: "match",
+    ...data,
+  });
+}
+
+export async function updateMatch(id: string, data: MatchType): Promise<MatchType & SanityFields> {
+  return client.patch(id).set(data).commit();
 }
